e6c2327c5a100cb9c857e60ef667e4ddfc84bd8f
Maja Kudlicka <maja.kudlicka@gmail.com>
2018-12-16 15:13:11 +0000

    feat - templates and styling

--- src/router.ts
+++ src/router.ts
@@ -5,19 +5,26 @@ import Home from './views/Home.vue';
 Vue.use(Router);
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
-    },
-  ],
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home,
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: () => import('@/views/Login.vue'),
+        },
+        {
+            path: '/register',
+            name: 'register',
+            component: () => import('@/views/Register.vue'),
+        },
+        {
+            path: '/settings',
+            name: 'settings',
+            component: () => import('@/views/Settings.vue'),
+        },
+    ],
 });
